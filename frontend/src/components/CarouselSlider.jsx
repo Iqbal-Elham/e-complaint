b90146134721c5dbc0281db6de68ecbe05fb63e8
iqbal-elham <iqbal.elham7@gmail.com>
2023-10-27 13:35:46 +0430

    Add the carousel slider

---
+++ frontend/src/components/CarouselSlider.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import slider1 from "../assets/slider1.jpeg";
+import slider2 from "../assets/slider2.jpeg";
+import slider3 from "../assets/slider3.jpeg";
+import slider4 from "../assets/slider4.jpeg";
+import slider5 from "../assets/slider5.jpg";
+import HeroSlider, { Overlay, Slide, MenuNav } from "hero-slider";
+
+const CarouselSlider = () => {
+  const options = {
+    items: 3, // Number of items to display at a time
+    autoplay: true,
+    autoplayTimeout: 3000, // Auto-play interval in milliseconds
+    nav: true, // Show navigation arrows
+  };
+
+  return (
+    <HeroSlider
+      height={"80vh"}
+      autoplay
+      controller={{
+        initialSlide: 1,
+        slidingDuration: 500,
+        slidingDelay: 100,
+        shouldAutoplay: true,
+        slidingAnimation: "right_to_left",
+        onSliding: (nextSlide) =>
+          console.debug("onSliding(nextSlide): ", nextSlide),
+        onBeforeSliding: (previousSlide, nextSlide) =>
+          console.debug(
+            "onBeforeSliding(previousSlide, nextSlide): ",
+            previousSlide,
+            nextSlide
+          ),
+        onAfterSliding: (nextSlide) =>
+          console.debug("onAfterSliding(nextSlide): ", nextSlide),
+      }}
+    >
+      {/* <Overlay>
+      <Wrapper>
+        <Title>Basic Setup</Title>
+        <Subtitle>
+          Check out the documentation for more advanced examples.
+        </Subtitle>
+      </Wrapper>
+    </Overlay> */}
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider1,
+        }}
+      />
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider5,
+        }}
+      />
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider4,
+        }}
+      />
+
+      <Slide
+        background={{
+          backgroundImageSrc: slider3,
+        }}
+      />
+
+    </HeroSlider>
+  );
+};
+
+export default CarouselSlider;
